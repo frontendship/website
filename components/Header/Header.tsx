d2cd2a151694f22fa8a4311543e85c493c409dfc
imhalid <imhalid@icloud.com>
2022-10-24 23:44:57 +0300

    feat: add tailwind

---
+++ components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import style from "./Header.module.css";
+
+const Header = () => {
+  return <div className={style.header}>Header</div>;
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Header from "../components/Header";
+import Header from "../components/Header/Header";
 const Home: NextPage = () => {
   return (
     <div>
