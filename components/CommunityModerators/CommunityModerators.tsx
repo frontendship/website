8e1995d2fb3c7f775b9e3ae0970845b5009ff64c
Veysel <veyselkartalms@gmail.com>
2022-10-29 22:15:56 +0300

    CommunityModerators folder added

---
+++ components/CommunityModerators/CommunityModerators.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./CommunityModerators.module.css";
+
+const CommunityModerators = () => {
+    return (
+        <div>
+            CommunityModerators
+        </div>
+    )
+}
+
+export default CommunityModerators
---
+++ components/CommunityModerators/ModeratorsCard/ModeratorCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./ModeratorCard.module.css";
+
+const ModeratorsCard = () => {
+    return (
+        <div>ModeratorsCard</div>
+    )
+}
+
+export default ModeratorsCard
