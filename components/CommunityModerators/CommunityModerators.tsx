1e546144407f545bdd4ee9c58a9cb9176cb413cf
Veysel <veyselkartalms@gmail.com>
2022-11-02 20:50:56 +0300

    card array and card component loop added

--- components/CommunityModerators/CommunityModerators.tsx
+++ components/CommunityModerators/CommunityModerators.tsx
@@ -3,6 +3,29 @@ import styles from "./CommunityModerators.module.css";
 import ModeratorsCard from './ModeratorsCard/ModeratorCard';
 
 const CommunityModerators = () => {
+    const moderatorCards = [
+        {
+            profileImage: "https://avatars.githubusercontent.com/u/74984741?v=4",
+            name: "Eray Gündoğmuş",
+            title: "Frontend Developer"
+        },
+        {
+            profileImage: "https://avatars.githubusercontent.com/u/25130814?v=4",
+            name: "Orhan Özkerçin",
+            title: "Frontend Developer"
+        },
+        {
+            profileImage: "https://avatars.githubusercontent.com/u/74984741?v=4",
+            name: "Eray Gündoğmuş",
+            title: "Frontend Developer"
+        },
+        {
+            profileImage: "https://avatars.githubusercontent.com/u/25130814?v=4",
+            name: "Orhan Özkerçin",
+            title: "Frontend Developer"
+        },
+    ];
+
     return (
         <div className={styles.container}>
             <div className={styles.titleContainer}>
@@ -11,10 +34,9 @@ const CommunityModerators = () => {
                 <p className={styles.desc}>Frontendship’in herkes için daha verimli ve eğlenceli bir topluluk olması için katkı sağlayan üyelerimiz.</p>
             </div>
             <div className={styles.cardContainer}>
-                <ModeratorsCard profileImage={"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} name={"Veysel Kartal"} title={"Frontend Developer"} />
-                <ModeratorsCard profileImage={"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} name={"Veysel Kartal"} title={"Frontend Developer"} />
-                <ModeratorsCard profileImage={"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} name={"Veysel Kartal"} title={"Frontend Developer"} />
-                <ModeratorsCard profileImage={"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"} name={"Veysel Kartal"} title={"Frontend Developer"} />
+                {moderatorCards.map((card) => (
+                    <ModeratorsCard profileImage={card.profileImage} name={card.name} title={card.title} />
+                ))}
             </div>
         </div>
     )
