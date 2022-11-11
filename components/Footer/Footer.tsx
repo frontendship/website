ff880a18327f7aa46ec49995c4bd4b5638e4b12f
Göktuğ <gokojapan5@gmail.com>
2022-11-11 17:57:45 +0300

    Add: module css added and footer fixed

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import logo from '../Icon/FrontendshipLogo.png';
 import Image from 'next/image';
 import styles from './Footer.module.css';
 import FooterData from './FooterData.json';
-import Link from 'next/link';
 
 const Footer = () => {
   const data = FooterData.map((data) => (
@@ -28,32 +27,26 @@ const Footer = () => {
   ));
 
   return (
-    <footer className="w-full bg-primary-blue-300">
-      <div className="flex lg:justify-between lg:mx-[135px] md:pt-[78px] md:pb-[85px] flex-col md:flex-row ">
-        <div className="text-center w-full pt-2 md:flex md:items-start  md:text-left md:pt-0 md:w-3/4 ">
-          {data}
-        </div>
-        <div className="flex flex-col items-center text-center mt-4 md:w-1/4 md:text-left md:mt-0 lg:w-1/3 ">
-          <div className="text-white text-lg md:w-full md:text-xl md:mb-5 ">
-            Subscribe Circle Newsletter
-          </div>
-          <div className="text-[#e7e7e7]  md:text-base md:mb-8 w-full">
+    <footer className={styles.container}>
+      <div className={styles.footerSection}>
+        <div className={styles.linksSide}>{data}</div>
+        <div className={styles.mailSide}>
+          <div className={styles.subsText}>Subscribe Circle Newsletter</div>
+          <div className={styles.subsMailText}>
             Subscribe to be the first one to know about updates. <br /> Enter
             your email
           </div>
-          <div className="relative inline-block w-72 h-12 mb-5 md:w-full ">
+          <div className={styles.mailInputContainer}>
             <input
               placeholder="Email Address"
               type={'email'}
-              className="rounded-xl border border-solid border-gray-500 text-white bg-transparent mt-2 h-full pl-2 w-full text-sm focus:outline-none  "
+              className={styles.mailInput}
             />
-            <button className="bg-white text-black rounded-xl h-full right-[1px] mt-2 absolute text-xs px-2 md:px-4 lg:px-6 ">
-              Subscribe
-            </button>
+            <button className={styles.mailInputButton}>Subscribe</button>
           </div>
         </div>
       </div>
-      <div className="bg-primary-blue-400 py-7 flex justify-center ">
+      <div className={styles.logoSection}>
         <Image src={logo} alt="logo" width="153px" height="40px" />
       </div>
     </footer>
